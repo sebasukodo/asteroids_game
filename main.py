3490839ebc5c63967fc8a47e1edeb0a17fdeb6d2
sebasukodo <sebastianlein@web.de>
2025-09-16 18:10:50 +0200

    adding player triangle model to screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -8,13 +9,22 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill([0,0,0])
+        player.draw(screen)
+
+
+
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
