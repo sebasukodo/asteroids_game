fbcaf137d741112f90da3b49eb63bc9144361cf4
sebasukodo <sebastianlein@web.de>
2025-09-17 13:03:33 +0200

    refactor code, game_run.py now executes gameplay, menu now shows exit button

--- game_menu.py
+++ game_menu.py
@@ -1,6 +1,6 @@
 import pygame
-import pygame.gfxdraw
 import pygame.freetype
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Rectangle:
@@ -9,9 +9,17 @@ class Rectangle:
         self.position = pygame.Vector2(x, y)
         self.width = width
         self.height = height
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
 
-    def draw(self, screen):
+    def draw(self, screen, text):
         pygame.draw.rect(screen, [255,255,255], self.rect, 2)
+
+        #render text
+        text_surface = self.font.render(text, True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=self.rect.center)
+
+        # draw text
+        screen.blit(text_surface, text_rect)
 
     def update(self, dt):
         mouse_click = pygame.mouse.get_pressed()
@@ -25,13 +33,14 @@ class Rectangle:
         return self.rect.collidepoint(point)
 
 def menu(screen):
-
     clock = pygame.time.Clock()
     dt = 0
 
     start_button_width = 300
     start_button_height = 100
     start_button = Rectangle((SCREEN_WIDTH - start_button_width)/2, (SCREEN_HEIGHT - start_button_height)/2, start_button_width, start_button_height)
+
+    exit_button = Rectangle((SCREEN_WIDTH - start_button_width)/2, (SCREEN_HEIGHT - start_button_height + 250)/2, start_button_width, start_button_height)    
 
     while True:
 
@@ -40,9 +49,14 @@ def menu(screen):
                 return False
 
         screen.fill([0,0,0])
+
         if start_button.update(dt):
             return
-        start_button.draw(screen)
+        start_button.draw(screen, "Start Game")
+
+        if exit_button.update(dt):
+            sys.exit()
+        exit_button.draw(screen, "Exit Game")
 
         pygame.display.flip()
         dt = clock.tick(60)/1000
---
+++ game_run.py
@@ -0,0 +1,75 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_MAX_LIVES
+from player import Player, Shot
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from hearts import Heart
+
+def run_game(screen):
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    hearts = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    Heart.containers = (hearts, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    AsteroidField()
+
+    # create all hearts
+    for i in range(PLAYER_MAX_LIVES):
+        Heart(100+i*20, 20, i+1)
+    for heart in hearts:
+        heart.update_hearts(player.lives)
+
+    dt = 0
+    clock = pygame.time.Clock()
+    
+    # Game Start
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill([0,0,0])
+
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+
+        for asteroid in asteroids:
+            if asteroid.check_collisions(player):
+                # destroy asteroid and reduce player life
+                asteroid.kill()
+                player.lives -= 1
+                if player.lives <= 0:
+                    print("Game over!")
+                    print(f"You destroyed {player.kill_count} asteroids while hitting {player.hit_count}")
+                    return
+
+                for heart in hearts:
+                    heart.update_hearts(player.lives)
+
+            for shot in shots:
+                # split or destroy asteroid and increase player counters accordingly after collision
+                if asteroid.check_collisions(shot):
+                    asteroid_dead = asteroid.split()
+                    shot.kill()
+
+                    player.hit_count += 1
+                    # after every 20 hits gain a life
+                    if player.hit_count % 20 == 0 and player.lives <= PLAYER_MAX_LIVES:
+                        player.lives += 1
+                        for heart in hearts:
+                            heart.update_hearts(player.lives)
+
+                    if asteroid_dead:
+                        player.kill_count += 1
+
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
